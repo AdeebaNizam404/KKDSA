f255613a0ccf80880adfc8ab8ae8ccc88cf051d1
Adeeba Nizam <adeebanizam63@gmail.com>
2025-05-15 01:14:21 +0530

    Added the PhonePAddQuestion

--- Ckeck.java
+++ Ckeck.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
 public class Ckeck {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-     int[] arr= new int[5];
-     int max=0;
-     for(int i=0;i<arr.length;i++){
-        arr[i]=in.nextInt();
-     }
-     for(int i=0;i<arr.length;i++){
-        if(arr[i]>max){
-            max=arr[i];
-        }
-     }
-     System.out.println("maximum:"+max);
-    in.close();
+   //      Scanner in = new Scanner(System.in);
+   //   int[] arr= new int[5];
+   //   int max=0;
+   //   for(int i=0;i<arr.length;i++){
+   //      arr[i]=in.nextInt();
+   //   }
+   //   for(int i=0;i<arr.length;i++){
+   //      if(arr[i]>max){
+   //          max=arr[i];
+   //      }
+   //   }
+   //   System.out.println("maximum:"+max);
+   //  in.close();
 
+    char ch = 'a'+1;
+ System.out.println(ch);
+    int c = (int)ch;
+ System.out.println(c);
      }
 }
