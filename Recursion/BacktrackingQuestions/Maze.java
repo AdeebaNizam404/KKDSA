2bbd6989b7fd264099a9da17e09e91a64730afc7
Adeeba Nizam <adeebanizam63@gmail.com>
2025-05-30 00:15:39 +0530

    Recursion completed

--- Recursion/BacktrackingQuestions/Maze.java
+++ Recursion/BacktrackingQuestions/Maze.java
@@ -2,7 +2,7 @@ package Recursion.BacktrackingQuestions;
 
 public class Maze {
     public static void main(String[] args){
-    System.out.println(count(3,3));
+    System.out.println(count(3,6));
     }
     static int count(int r,int c){
         if(r==1||c==1){
